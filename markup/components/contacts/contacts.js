bc8693274a33383124af7c556ae8adac51eeac1d
Developer_Full <salvatore-galiani@mail.ru>
2018-06-28 16:25:44 +0300

    Contacts

--- markup/components/contacts/contacts.js
+++ markup/components/contacts/contacts.js
@@ -14,6 +14,12 @@ function init(){
     myMap.behaviors.disable('scrollZoom') 
     myPlacemark = new ymaps.Placemark([59.919865, 30.466704], { 
         hintContent: 'Санкт-Петербург!', 
+    },
+    {
+       iconLayout: 'default#image',
+       // Custom image for the placemark icon.
+       iconImageHref: '../static/img/assets/contacts/marker.svg',
+       iconImageSize: [50, 50],
     });
 
     myMap.geoObjects.add(myPlacemark);
