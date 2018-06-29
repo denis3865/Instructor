13e2cd10327bbf62518c29f33affd2a5dd211477
Developer_Full <salvatore-galiani@mail.ru>
2018-06-29 08:25:30 +0300

    CL About

--- markup/components/contacts/contacts.js
+++ markup/components/contacts/contacts.js
@@ -18,7 +18,7 @@ function init(){
     {
        iconLayout: 'default#image',
        // Custom image for the placemark icon.
-       iconImageHref: '../static/img/assets/contacts/marker.svg',
+       iconImageHref: 'static/img/assets/contacts/marker.svg',
        iconImageSize: [50, 50],
     });
 
